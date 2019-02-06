92aefc9f3d105e195016bf52c5b83375582b6419
qpjakewaggqp <32466459+qpjakewaggqp@users.noreply.github.com>
2019-02-06 15:55:46 -0500

    Forgot to remove the token, oof

--- bot src/Index.js
+++ bot src/Index.js
@@ -39,7 +39,7 @@ var autoAssignedRole;
 var currentMembers = [];
 var currentUserIDs = [];
 
-const Token = "NTA0ODE5NzkxMDAyMDc1MTM3.DxCCBQ.WIFKMy7TPV35ZwSwdUuDZvky_WE";
+const Token = "";
 
 const prefix = "//"
 
